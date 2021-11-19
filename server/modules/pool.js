eb669931a769102e87e94f916984305662ecf919
Michael Huso <husomichael@gmail.com>
2021-11-19 12:27:44 -0600

    setup pool.js

---
+++ server/modules/pool.js
@@ -0,0 +1,16 @@
+const pg = require('pg');
+const Pool = pg.Pool;
+const pool = new Pool({
+    database: 'to-do-list',
+    host: 'localhost'
+});
+
+pool.on('connect', () =>{
+    console.log('Postgresql connected');
+});
+
+pool.on('error', () =>{
+    console.log('Error with postgres pool', error);
+});
+
+module.exports = pool;
