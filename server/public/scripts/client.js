a4bc73efde4899c55019a44661d926c2b88b88ef
Michael Huso <husomichael@gmail.com>
2021-11-19 13:47:49 -0600

    added table to html

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,3 +5,13 @@ $(document).ready(readyNow);
 function readyNow(){
     console.log('jq');
 }
+
+//GET Route render todo list
+
+// DELETE route delete a task
+
+// POST route create a task
+
+// PUT route complete a task
+
+//PUT route importance marker
