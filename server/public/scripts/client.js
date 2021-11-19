26fe345ef940a363ac9a477524148b06079a143e
Michael Huso <husomichael@gmail.com>
2021-11-19 14:15:16 -0600

    renderList get route working and rendering todoList to the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,9 +4,33 @@ $(document).ready(readyNow);
 
 function readyNow(){
     console.log('jq');
+    renderList();
 }
 
 //GET Route render todo list
+function renderList(){
+    console.log('in getList');
+
+    $.ajax({
+        method: 'GET',
+        url: '/todolist'
+    }).then((response) =>{
+        const todoList = response;
+        $('#view-todolist').empty();
+        console.log('GET /todolist response:', todoList);
+        for(let todo of todoList){
+            $('#view-todolist').append(`
+                <tr>
+                    <td>${todo.importance}</td>
+                    <td>${todo.task}</td>
+                    <td>${todo.completed}</td>
+                </tr>
+            `);
+        }
+    }).catch((error) =>{
+        console.log('/todolist GET error:', error);
+    })
+}//END getList
 
 // DELETE route delete a task
 
--- server/routes/todolistRoute.js
+++ server/routes/todolistRoute.js
@@ -5,7 +5,16 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 //GET route
-
+router.get('/', (req, res) =>{
+    const sqlText = 'SELECT * FROM todo;';
+    pool.query(sqlText)
+        .then((dbRes) =>{
+            const listFromDB = dbRes.rows;
+            res.send(listFromDB);
+        }).catch((dbError) =>{
+            console.error('Get route error:', dbError);
+        });
+});
 //POST route
 
 //DELETE route
